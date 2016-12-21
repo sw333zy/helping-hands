4642bd25628bdba153e6048d0986dfc9b9a0b359
J Ridgeway <sw333zy@gmail.com>
2016-12-21 14:15:38 -0500

    permantant map up

--- build/js/app.js
+++ build/js/app.js
@@ -76,4 +76,25 @@
       };
 
     }
+
+}());
+
+(function() {
+  'use strict';
+
+  angular.module('helpingHands')
+    .directive('mapbox', MapBox);
+
+
+    function MapBox() {
+      return {
+        restrict: 'EA',
+        link: function (scope, element) {
+          L.mapbox.accessToken = 'pk.eyJ1Ijoic3czMzN6eSIsImEiOiJjaXdzMnluaXUxM3hwMnRzN3I4cHl2bnBnIn0.MhLpogI8pC6zp8qUBMID0w';
+          L.mapbox.map(element[0], 'mapbox.streets')
+            .setView([38.9072, -77.0369], 9);
+        }
+      };
+    }
+
 }());
