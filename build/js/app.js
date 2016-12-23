77938b505a16539cc739278a0868f0cc3724f79c
J Ridgeway <sw333zy@gmail.com>
2016-12-23 13:54:49 -0500

    map locations loaded through api

--- build/js/app.js
+++ build/js/app.js
@@ -175,26 +175,30 @@
   angular.module('helpingHands')
   .directive('mapbox', MapBox);
 
+  MapBox.$inject = ['DcOpenDataService'];
 
-  function MapBox() {
+  function MapBox(DcOpenDataService) {
     return {
       restrict: 'EA',
       link: function (scope, element) {
         L.mapbox.accessToken = 'pk.eyJ1Ijoic3czMzN6eSIsImEiOiJjaXdzMnluaXUxM3hwMnRzN3I4cHl2bnBnIn0.MhLpogI8pC6zp8qUBMID0w';
 
         var map = L.mapbox.map(element[0], 'mapbox.streets')
-        .setView([38.9, -77], 12)
-        .addLayer(L.mapbox.tileLayer('mapbox.streets'));
+          .setView([38.9, -77], 12)
+          .addLayer(L.mapbox.tileLayer('mapbox.streets'));
 
-        L.mapbox.featureLayer('http://opendata.dc.gov/datasets/87c5e68942304363a4578b30853f385d_25.geojson')
-        .on('ready', function(e) {
+        DcOpenDataService.getServices()
+        .then(function handleSuccess(data){
+          console.log(data, 'dc data from call');
 
-          var clusterGroup = new L.MarkerClusterGroup();
-          e.target.eachLayer(function(layer) {
-            clusterGroup.addLayer(layer);
-          });
-          map.addLayer(clusterGroup);
+          map.featureLayer.setGeoJSON(data);
+
+        })
+        .catch(function handleError(err){
+           console.log(err);
         });
+
+
       }
     };
   }
