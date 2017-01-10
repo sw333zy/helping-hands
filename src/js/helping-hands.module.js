74dc147ce3b27259d4b343d7dc02e7299c2350d8
J Ridgeway <sw333zy@gmail.com>
2017-01-10 11:38:13 -0500

    removed address controller and linked map directly to home state sref

--- src/js/helping-hands.module.js
+++ src/js/helping-hands.module.js
@@ -11,18 +11,17 @@
 
     $stateProvider
       .state({
-        name: 'home', //switch back to home
-        url: '', //switch back to blank
+        name: 'home',
+        url: '',
         templateUrl: 'views/home.template.html',
-        controller: 'AddressController',
-        controllerAs: 'AddressController'
+      
 
 
       })
 
       .state({
-        name: 'addressMapView', //switch back to addressMapView
-        url: '/addressMapView', //switch back to /adress
+        name: 'addressMapView',
+        url: '/addressMapView',
         templateUrl: 'views/map-view.template.html',
         controller: 'MapViewController',
         controllerAs: 'mapView',
